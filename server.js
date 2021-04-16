6bff97615db859c1ffff9bccb0ca1fbf3a976382
Julian Almendarez <ppalmendarez@yahoo.com>
2021-04-15 23:54:01 -0500

    adjusted code

--- server.js
+++ server.js
@@ -6,11 +6,17 @@ const path = require('path');
 // Sets up the Express App
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3001;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(express.static('public'))
+
+app.get('/index', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
+
 
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
